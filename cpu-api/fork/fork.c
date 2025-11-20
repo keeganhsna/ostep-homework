acc1d13436ce80155836953ddff4b855047023cb
keeganhsna <keeganhsna@gmail.com>
2025-11-19 17:57:51 -0800

    first q

--- cpu-api/fork/fork.c
+++ cpu-api/fork/fork.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <wait.h>
 
+int main(void){
+
+	int x = 100;
+	
+	pid_t rc = fork();
+	
+	if (rc < 0){
+		fprintf(stderr,"fork failed");
+		exit(1);
+	} else if (rc == 0) {
+		fprintf(stdout, "child: pre-subtract: %d\n",x);
+		x--;
+		fprintf(stdout, "child: post-subtract: %d\n",x);
+	}else{
+		
+		fprintf(stdout, "parent: print in else %d\n",x);
+	}
+	return 0;
+}
